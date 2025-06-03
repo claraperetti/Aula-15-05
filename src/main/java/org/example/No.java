abc302545e51bf8b026c64ca19d7b27a2bd04efd
Clara L Peretti <claralobrigatte@gmail.com>
2025-06-03 19:56:48 -0300

    mudanca-no-arvore-avl

--- src/main/java/org/example/No.java
+++ src/main/java/org/example/No.java
@@ -3,10 +3,12 @@ package org.example;
 public class No {
     public String valor;
     public No esquerda, direita;
+    public int altura;
 
     public No (String valor) {
     this.valor = valor;
     esquerda = direita = null;
+    this.altura = 1;
     }
 
 
