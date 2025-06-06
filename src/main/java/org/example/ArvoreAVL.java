682de60d799c56cbc784e09628b3693917752afe
Clara L Peretti <claralobrigatte@gmail.com>
2025-06-05 22:01:44 -0300

    metodos-arvore-avl

--- src/main/java/org/example/ArvoreAVL.java
+++ src/main/java/org/example/ArvoreAVL.java
@@ -21,4 +21,49 @@ public class ArvoreAVL {
         node.altura = 1 + Math.max(alturaEsquerda, alturaDireita);
         return node.altura;
     }
+
+    public No rotacaoDireita(No node){
+        No x = node.esquerda;
+        No t2 = x.direita;
+
+        x.direita = node;
+        node.esquerda = t2;
+
+        x.altura = Math.max(altura(x.esquerda), altura(x.direita)) + 1;
+        node.altura = Math.max(altura(node.esquerda), altura(node.direita)) + 1;
+
+        return node;
+    }
+
+    public No rotacaoEsquerda(No node){
+        No y = node.direita;
+        No t2 = y.esquerda;
+
+        y.esquerda =  node;
+        node.direita =  t2;
+
+        node.altura = Math.max(altura(x.esquerda), altura(x.direita)) + 1;
+        y.altura = Math.max(altura(node.esquerda), altura(node.direita)) + 1;
+
+    }
+
+    public No inserir(No node, int chave){
+
+        if (node == null)
+            return new No(chave);
+
+        if (chave < node.chave)
+            node.esquerda =  inserir((node.esquerda, chave);
+        else if (chave > node.chave)
+            node.direita = inserir(node.direita, chave);
+        else
+            return node;
+
+        node.altura = 1 + Math.max(altura(node.esquerda), calcularAltura(node.direita));
+
+        int balanceamento = fatorBalanceamento(node);
+
+        if balanceamento > 1 && chave < node.esquerda.chave)
+            return rotacaoDireita(node);
+    }
 }
