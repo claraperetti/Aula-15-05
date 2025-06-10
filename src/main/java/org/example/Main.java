a7b1105ff0dc6c25b89511c60a2824ba90d327bd
Clara L Peretti <claralobrigatte@gmail.com>
2025-06-10 20:40:47 -0300

    main-arvore-avl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        ArvoreAVL arvore = new ArvoreAVL();
+
+        int[] chaves = {10, 20, 30, 40, 50, 25};
+
+        for (int chave : chaves) {
+            arvore.noRaiz = arvore.inserir(arvore.noRaiz, chave);
+        }
+
+        System.out.println("`Percursp em ordem da árvore AVL");
+        arvore.percursoEmOrdem(arvore.noRaiz);
     }
 }
